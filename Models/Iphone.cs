8474c57b8a8b853f0e11df4bf589843972e54f20
Gustavo Gosser Rodrigues <gosser07@gmail.com>
2023-10-06 15:37:45 -0300

    Feito

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string iMEI, string  modelo, int memoria) : base(numero, iMEI, modelo, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o app {nomeApp} para Iphone");
+        
+    }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string iMEI, string  modelo, int memoria) : base(numero, iMEI, modelo, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o app {nomeApp} para nokia");
+        }
     }
 }
