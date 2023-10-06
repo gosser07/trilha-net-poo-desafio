1f54367cee9c9bcf96a15bc19d48273a8ffdff6d
Gustavo Gosser Rodrigues <gosser07@gmail.com>
2023-10-06 15:31:47 -0300

    Feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia (numero: "1234", iMEI: "56789", modelo: "tijolo", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("apptestenokia");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone (numero: "4321", iMEI: "98765", modelo: "Pro Max", memoria: 16);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("apptesteIphone");
